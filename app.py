471beeec6fb8ee821bcfb6420dcc7e9d827e064c
trishikesh008-web <t.rishikesh.008@gmail.com>
2026-06-11 21:07:28 +0530

    Implemented Dijkstra shortest path algorithm

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
-import random
+import time
 
-def generate_graph(vertices):
+def dijkstra(graph, source):
 
-    graph = [
-        [0 for _ in range(vertices)]
-        for _ in range(vertices)
-    ]
+    n = len(graph)
 
-    for i in range(vertices):
-        for j in range(vertices):
+    dist = [float('inf')] * n
 
-            if i != j:
-                graph[i][j] = random.randint(1, 20)
+    visited = [False] * n
 
-    return graph
+    dist[source] = 0
+
+    start = time.perf_counter()
+
+    for _ in range(n):
+
+        min_dist = float('inf')
+        u = -1
+
+        for i in range(n):
+
+            if not visited[i] and dist[i] < min_dist:
+
+                min_dist = dist[i]
+                u = i
+
+        visited[u] = True
+
+        for v in range(n):
+
+            if (
+                not visited[v]
+                and graph[u][v]
+                and dist[u] + graph[u][v] < dist[v]
+            ):
+
+                dist[v] = dist[u] + graph[u][v]
+
+    end = time.perf_counter()
+
+    return round((end - start) * 1000, 5)
